914c2fbf8b84f26d8112bc1476d8755f2cf88fcf
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-02-12 16:41:45 +0000

    chore: remove references to ios

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import {
+  NativeModulesProxy,
+  EventEmitter,
+  Subscription,
+} from "expo-modules-core";
 
 // Import the native module. On web, it will be resolved to ExpoAndroidAccountManager.web.ts
 // and on native platforms to ExpoAndroidAccountManager.ts
-import ExpoAndroidAccountManagerModule from './ExpoAndroidAccountManagerModule';
-import ExpoAndroidAccountManagerView from './ExpoAndroidAccountManagerView';
-import { ChangeEventPayload, ExpoAndroidAccountManagerViewProps } from './ExpoAndroidAccountManager.types';
+import {
+  ChangeEventPayload,
+  ExpoAndroidAccountManagerViewProps,
+} from "./ExpoAndroidAccountManager.types";
+import ExpoAndroidAccountManagerModule from "./ExpoAndroidAccountManagerModule";
 
 // Get the native constant value.
 export const PI = ExpoAndroidAccountManagerModule.PI;
@@ -17,10 +23,15 @@ export async function setValueAsync(value: string) {
   return await ExpoAndroidAccountManagerModule.setValueAsync(value);
 }
 
-const emitter = new EventEmitter(ExpoAndroidAccountManagerModule ?? NativeModulesProxy.ExpoAndroidAccountManager);
+const emitter = new EventEmitter(
+  ExpoAndroidAccountManagerModule ??
+    NativeModulesProxy.ExpoAndroidAccountManager,
+);
 
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+export function addChangeListener(
+  listener: (event: ChangeEventPayload) => void,
+): Subscription {
+  return emitter.addListener<ChangeEventPayload>("onChange", listener);
 }
 
-export { ExpoAndroidAccountManagerView, ExpoAndroidAccountManagerViewProps, ChangeEventPayload };
+export { ExpoAndroidAccountManagerViewProps, ChangeEventPayload };
