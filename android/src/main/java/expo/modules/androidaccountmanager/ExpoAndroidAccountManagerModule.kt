44803b36c354260d5f3e7026e1ccb90f022ea7fe
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-02-13 12:15:12 +0000

    feat: add initial getAccounts function

--- android/src/main/java/expo/modules/androidaccountmanager/ExpoAndroidAccountManagerModule.kt
+++ android/src/main/java/expo/modules/androidaccountmanager/ExpoAndroidAccountManagerModule.kt
@@ -1,47 +1,24 @@
 package expo.modules.androidaccountmanager
 
+import android.accounts.AccountManager
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoAndroidAccountManagerModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoAndroidAccountManager')` in JavaScript.
-    Name("ExpoAndroidAccountManager")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
+  private val context
+    get() = requireNotNull(appContext.reactContext)
+
+  override fun definition() = ModuleDefinition {
+    Name("ExpoAndroidAccountManager")
 
     // Defines event names that the module can send to JavaScript.
     Events("onChange")
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(ExpoAndroidAccountManagerView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: ExpoAndroidAccountManagerView, prop: String ->
-        println(prop)
-      }
+    Function("getAccounts") {
+      val accountManager = AccountManager.get(context)
+      val accounts = accountManager.accounts.map { it.name }
+      accounts.toTypedArray()
     }
   }
 }
