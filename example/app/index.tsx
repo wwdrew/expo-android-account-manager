65e06ec167f58055c875af28593d20e050526df3
Drew Miller <49833875+wwdrew@users.noreply.github.com>
2024-02-19 16:58:31 +0000

    improve layouts

--- example/app/index.tsx
+++ example/app/index.tsx
@@ -2,13 +2,34 @@ import {
   getAccounts,
   addAccountExplicitly,
   Account,
-  removeAccount,
 } from "expo-android-account-manager";
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const ACCOUNT_TYPE = "expo.modules.androidaccountmanager.example";
+
+function CreateRandomAccount({ callback }: { callback: () => void }) {
+  function createRandomAccount() {
+    const random = Math.random().toString().substring(2, 8);
+
+    try {
+      addAccountExplicitly(
+        {
+          name: `account-name-${random}`,
+          type: ACCOUNT_TYPE,
+        },
+        "password",
+      );
+
+      callback();
+    } catch (error) {
+      console.log({ error });
+    }
+  }
+
+  return <Text onPress={createRandomAccount}>Create</Text>;
+}
 
 export default function AccountManagerHomeScreen() {
   const [accounts, setAccounts] = useState<Account[]>([]);
@@ -17,48 +38,26 @@ export default function AccountManagerHomeScreen() {
     setAccounts(getAccounts());
   }, []);
 
-  function createRandomAccount() {
-    console.log("createRandomAccount");
-    const random = Math.random().toString().substring(2, 8);
-
-    try {
-      const success = addAccountExplicitly(
-        {
-          name: `account-name-${random}`,
-          type: ACCOUNT_TYPE,
-        },
-        "password",
-      );
-      console.log(`added ${random}: ${success}`);
-      console.log("getAccounts: ", getAccounts());
-      setAccounts(getAccounts());
-    } catch (error) {
-      console.log({ error });
-    }
-  }
-
-  function handleRemoveAccount(accountName: string) {
-    console.log("remove account: ", accountName);
-    try {
-      removeAccount({
-        name: accountName,
-        type: ACCOUNT_TYPE,
-      });
-
-      setAccounts(getAccounts());
-    } catch (error) {
-      console.log({ error });
-    }
-  }
-
   return (
     <View style={styles.container}>
-      <Button onPress={createRandomAccount} title="Create random account" />
-      <Text>Accounts:</Text>
+      <Stack.Screen
+        options={{
+          title: "Accounts",
+          headerRight: () => (
+            <CreateRandomAccount callback={() => setAccounts(getAccounts())} />
+          ),
+        }}
+      />
       {accounts.length === 0 && <Text>No accounts</Text>}
       {accounts.map((account) => (
-        <Link href={`/account/${account.name}`} key={account.name}>
-          <Text>{account.name}</Text>
+        <Link
+          key={account.name}
+          href={{
+            pathname: "/account/[name]",
+            params: { name: account.name },
+          }}
+        >
+          {account.name}
         </Link>
       ))}
     </View>
@@ -69,7 +68,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
